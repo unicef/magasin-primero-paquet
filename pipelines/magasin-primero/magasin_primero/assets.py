bb5757621b861d1d62dedbd68e7c8ed191304c60
merlos <404446+merlos@users.noreply.github.com>
2024-10-23 14:11:56 +0300

    first working PoC for the data pipeline

--- pipelines/magasin-primero/magasin_primero/assets.py
+++ pipelines/magasin-primero/magasin_primero/assets.py
@@ -11,21 +11,44 @@ def cases() -> DataFrame:
   # Load from API
   PRIMERO_USER= "primero"
   PRIMERO_PASSWORD='primer0!'
-  PRIMERO_API_URL='http://localhost/api/v2'
+  PRIMERO_API_URL='http://localhost/api/v2/'
 
   print("Setting up connection to Primero API... ")
-  primero = PrimeroAPI(PRIMERO_USER, PRIMERO_PASSWORD, PRIMERO_API_URL) 
-  
+  primero = PrimeroAPI(PRIMERO_USER, PRIMERO_PASSWORD, PRIMERO_API_URL)
+  print('Primero API library version', primero.version())
+  print('Primero Server version', primero.get_server_version())
+
   print("Getting cases... ")
   df = primero.get_cases()
-  print("------ cases ------")
   print(df)
-  print("------ cases ------")
-  
   fs= fsspec.filesystem('s3')
-  with fs.open('/primero/cases.parquet','wb') as f:
+  with fs.open('/primero/cases/cases.parquet','wb') as f:
     df.to_parquet(f)
   return df
+
+
+@asset
+def incidents() -> DataFrame:
+  """ Retrieves cases from Primero API """
+  # Load from API
+  PRIMERO_USER= "primero"
+  PRIMERO_PASSWORD='primer0!'
+  PRIMERO_API_URL='http://localhost/api/v2/'
+
+  print("Setting up connection to Primero API... ")
+  primero = PrimeroAPI(PRIMERO_USER, PRIMERO_PASSWORD, PRIMERO_API_URL)
+  print('Primero API library version', primero.version())
+  print('Primero Server version', primero.get_server_version())
+
+  print("Getting cases... ")
+  df = primero.get_incidents()
+  print(df)
+  fs= fsspec.filesystem('s3')
+  with fs.open('/primero/cases/cases.parquet','wb') as f:
+    df.to_parquet(f)
+  return df
+
+
 
 @asset
 def reports()-> Dict:
@@ -37,11 +60,14 @@ def reports()-> Dict:
   PRIMERO_API_URL='http://localhost/api/v2/'
 
   primero = PrimeroAPI(PRIMERO_USER, PRIMERO_PASSWORD, PRIMERO_API_URL) 
-  fs= fsspec.filesystem('s3')
   
   reports = primero.get_reports()
-  for report in reports:  
-    with fs.open(f'/primero/report-{report.id}-{report.slug}.parquet','wb') as f:
+
+  fs= fsspec.filesystem('s3')
+  for report in reports.values():  
+    if report is None:
+      continue
+    with fs.open(f'/primero/report-{report.id}-{report.slug}/report.parquet','wb') as f:
       report.to_pandas().to_parquet(f)
     
   return reports
