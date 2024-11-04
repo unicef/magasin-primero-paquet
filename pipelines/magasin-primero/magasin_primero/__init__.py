5deaf5200d167d99eeb4ba65aee1cc059c1a5134
merlos <merlos@gmail.com>
2024-11-04 06:07:10 +0300

    Add scheduler

--- pipelines/magasin-primero/magasin_primero/__init__.py
+++ pipelines/magasin-primero/magasin_primero/__init__.py
@@ -1,9 +1,15 @@
+import os 
+
 from dagster import Definitions, load_assets_from_modules, EnvVar
 from .resources import PrimeroAPIResource
 from . import assets
+from dagster import load_asset_checks_from_modules
+from dagster import ScheduleDefinition, DefaultScheduleStatus, AssetSelection
 
 # Load all assets from the assets module
 all_assets = load_assets_from_modules([assets], group_name="primero")
+
+all_asset_checks = load_asset_checks_from_modules([assets])
 
 # Load from env the primero settings
 primero_user = EnvVar("PRIMERO_USER")
@@ -15,7 +21,34 @@ primero_api_resource = PrimeroAPIResource(primero_user=primero_user,
                                           primero_password=primero_password,
                                           primero_api_url=primero_api_url)
 
+
+environment = os.getenv("ENVIRONMENT")
+
+print("****************** Environment")
+print(environment)
+print("****************** Environment")
+
+
+# Default schedule for the job every day at midnight
+schedule = ScheduleDefinition(
+    name="daily_job", 
+    target=all_assets,
+    cron_schedule="0 0 * * *",
+    default_status= DefaultScheduleStatus.RUNNING)
+
+if environment == 'dev':
+    
+    schedule = ScheduleDefinition(
+    name="dev_minute_job", 
+    target=all_assets,
+    cron_schedule="* * * * *",
+    #default_status= DefaultScheduleStatus.RUNNING
+    )
+
+
 defs = Definitions(
     assets=all_assets,
+    asset_checks=all_asset_checks,
+    schedules=[schedule],
     resources={"primero_api": primero_api_resource}
 )
